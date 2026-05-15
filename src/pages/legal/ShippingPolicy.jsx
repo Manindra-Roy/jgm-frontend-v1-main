4df6dd59686d4184623f32fa7eec8bd8652d96a7
Manindra-Roy <anonymous.secret.in@gmail.com>
2026-05-15 07:52:37 +0000

    feat: add ReturnsPolicy and ShippingPolicy pages to legal section

--- src/pages/legal/ShippingPolicy.jsx
+++ src/pages/legal/ShippingPolicy.jsx
@@ -6,6 +6,7 @@ export default function ShippingPolicy() {
             <p>The orders for the user are shipped through registered domestic courier companies and/or speed post only. Orders are shipped within 7 days from the date of the order and/or payment or as per the delivery date agreed at the time of order confirmation and delivering of the shipment, subject to courier company / post office norms.</p>
             
             <p>Platform Owner shall not be liable for any delay in delivery by the courier company / postal authority. Delivery of all orders will be made to the address provided by the buyer at the time of purchase. Delivery of our services will be confirmed on your email ID as specified at the time of registration.</p>
+            <p>Order will be delivered with in 7 to 10 days</p>
 
             <p>If there are any shipping cost(s) levied by the seller or the Platform Owner (as the case be), the same is not refundable.</p>
         </LegalPageLayout>
